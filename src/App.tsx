c00cbb5cdbf7be97916a10e62ef8f18627c4dbf8
Jaime Mejia <jaimemejia1003@gmail.com>
2026-06-13 21:07:19 -0500

    feat: add automatic update checking via GitHub releases with in-app notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import AppRouter from "./router/AppRouter";
 import { Toaster } from "./components/ui/sonner";
 import DebugMenu from "./components/debug/DebugMenu";
+import { useUpdateChecker } from "./hooks/useUpdateChecker";
 
 function App() {
+  useUpdateChecker();
+
   return (
     <main className="flex w-full h-screen">
       <AppRouter />
